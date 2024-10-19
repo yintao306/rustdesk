46d810741d1eeb3aed2e46d290c5c7b6039237a6
yintao306 <124324065+yintao306@users.noreply.github.com>
2024-10-19 18:54:31 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.yintao2030.pp.ua"];
-pub const PUBLIC_RS_PUB_KEY: &str = "iEyskoaYRwLDy5+0qNDqkbPdpxr0kXRSZxNjEsqykyE=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["nas.yintao2030.pp.ua"];
+pub const PUBLIC_RS_PUB_KEY: &str = "K0BOFsYST3zoxHx0wqiYCsOK4vMObOAXdOi6Fz+B+7w=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
